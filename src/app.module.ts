2acee0f3404266feab451cb809b46c6e41308bfc
Alejandro <alejoestradam@yahoo.com>
2023-01-24 08:29:41 -0500

    cascaron de proyecto de todos

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { join } from 'path';
+
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ItemsModule } from './items/items.module';
+
+
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+     // debug: false,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+      autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+      }),
+    ItemsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
