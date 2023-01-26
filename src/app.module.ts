c4dda1f6b085ac3a8f5b86c56c266ded186a2a21
Alejandro <alejoestradam@yahoo.com>
2023-01-26 08:05:08 -0500

    se agrega docker-compose con postgres y se hizo conexion con la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,14 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { ItemsModule } from './items/items.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
      // debug: false,
@@ -17,7 +20,18 @@ import { ItemsModule } from './items/items.module';
       plugins: [ApolloServerPluginLandingPageLocalDefault],
       autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
       }),
-    ItemsModule,
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [],
+        synchronize: true,
+        autoLoadEntities: true
+      }),
+      ItemsModule
   ],
   controllers: [],
   providers: [],
